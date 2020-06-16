58b2c9b91508a41d883e1ae4c1ade30abb563ecf
Konstantin <Satana.Konst@gmail.com>
2020-06-16 14:42:05 +0300

    Change autoload classses and change structure

--- src/NextCloudWebDavSdk.php
+++ src/NextCloudWebDavSdk.php
@@ -2,15 +2,9 @@
 
 namespace NextCloudWebDavSdk;
 
-use NextCloudWebDavSdk\OCS\Share;
-use NextCloudWebDavSdk\WebDav\WebDav;
 
 class NextCloudWebDavSdk
 {
-    private $baseUrl;
-    private $login;
-    private $pass;
-
     public $server;
     public $share;
 
@@ -20,16 +14,14 @@ class NextCloudWebDavSdk
      * @param $login
      * @param $pass
      */
-    public function __construct($baseUrl,$login,$pass)
+    public function __construct($host,$login,$pass)
     {
-        $this->baseUrl = $baseUrl;
+        $this->baseUrl = $host;
         $this->login = $login;
         $this->pass = $pass;
 
-//        $this->server = new Server($baseUrl,$login,$pass);
-//        $this->share = new Share($login,$pass);
+        $this->server = new WebDav($host,$login,$pass);
+        $this->share = new Share($host,$login,$pass);
     }
-
-
 
 }
